f79550681851f591bfdd036ae2815fdbf5411f2a
Bluemangoo <chenfy2006@qq.com>
2024-07-15 12:41:40 +0800

    add mob copy

--- hypertension-spigot/src/main/java/net/bluemangoo/hypertension/spigot/Hypertension.java
+++ hypertension-spigot/src/main/java/net/bluemangoo/hypertension/spigot/Hypertension.java
@@ -3,7 +3,7 @@ package net.bluemangoo.hypertension.spigot;
 import net.bluemangoo.hypertension.spigot.listener.FallDamage;
 import net.bluemangoo.hypertension.spigot.listener.Miss;
 import net.bluemangoo.hypertension.spigot.listener.PvpPunishment;
-import net.bluemangoo.hypertension.spigot.listener.SilverfishCopy;
+import net.bluemangoo.hypertension.spigot.listener.MobCopy;
 import org.bukkit.plugin.java.JavaPlugin;
 
 
@@ -14,6 +14,6 @@ public final class Hypertension extends JavaPlugin {
         getServer().getPluginManager().registerEvents(new Miss(this), this);
         getServer().getPluginManager().registerEvents(new FallDamage(this), this);
         getServer().getPluginManager().registerEvents(new PvpPunishment(this), this);
-        getServer().getPluginManager().registerEvents(new SilverfishCopy(this), this);
+        getServer().getPluginManager().registerEvents(new MobCopy(this), this);
     }
 }
--- hypertension-spigot/src/main/java/net/bluemangoo/hypertension/spigot/listener/MobCopy.java
+++ hypertension-spigot/src/main/java/net/bluemangoo/hypertension/spigot/listener/MobCopy.java
@@ -1,24 +1,37 @@
 package net.bluemangoo.hypertension.spigot.listener;
 
+import com.google.common.collect.ImmutableList;
 import net.bluemangoo.hypertension.spigot.Hypertension;
-import org.bukkit.entity.Silverfish;
+import org.bukkit.entity.EntityType;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDeathEvent;
 
 import java.util.Random;
 
-public class SilverfishCopy implements Listener {
+public class MobCopy implements Listener {
     protected final Hypertension plugin;
     protected final Random random = new Random();
+    protected final ImmutableList<EntityType> respawns =
+        ImmutableList.of(
+            EntityType.SILVERFISH,
+            EntityType.CREEPER,
+            EntityType.ENDERMITE,
+            EntityType.PHANTOM,
+            EntityType.SLIME,
+            EntityType.SPIDER,
+            EntityType.CAVE_SPIDER,
+            EntityType.MAGMA_CUBE,
+            EntityType.GHAST
+        );
 
-    public SilverfishCopy(Hypertension plugin) {
+    public MobCopy(Hypertension plugin) {
         this.plugin = plugin;
     }
 
     @EventHandler
     public void onEntityDeathEvent(EntityDeathEvent event) {
-        if (event.getEntity() instanceof Silverfish) {
+        if (this.respawns.contains(event.getEntity().getType())) {
             if (this.random.nextInt(10) < 3) {
                 event.getEntity().getWorld().spawnEntity(event.getEntity().getLocation(), event.getEntity().getType());
             } else if (this.random.nextInt(10) < 3) {
